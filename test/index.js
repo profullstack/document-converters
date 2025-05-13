b9085596277b29d8a9a61a01559908f26855f8a4
Anthony Ettinger <anthony@chovy.com>
2025-05-12 20:45:40 -0700

    tests

--- test/index.js
+++ test/index.js
@@ -1,28 +1,14 @@
 /**
- * Basic tests for @profullstack/document-converters
+ * Main test file for @profullstack/document-converters
+ *
+ * This file imports and runs all test files
  */
 
-import { expect } from 'chai';
-import fs from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-describe('@profullstack/document-converters', function() {
-  it('should have a package.json file', function() {
-    const pkgPath = path.join(__dirname, '../package.json');
-    expect(fs.existsSync(pkgPath)).to.be.true;
-    
-    const pkgContent = fs.readFileSync(pkgPath, 'utf8');
-    const pkg = JSON.parse(pkgContent);
-    expect(pkg).to.be.an('object');
-    expect(pkg.name).to.equal('@profullstack/document-converters');
-  });
-  
-  it('should have a src directory', function() {
-    const srcDir = path.join(__dirname, '../src');
-    expect(fs.existsSync(srcDir)).to.be.true;
-  });
-});
+// Import all test files
+import './converters/index.test.js';
+import './converters/markdown.test.js';
+import './converters/pdf.test.js';
+import './converters/doc.test.js';
+import './converters/excel.test.js';
+import './converters/ppt.test.js';
+import './converters/epub.test.js';
